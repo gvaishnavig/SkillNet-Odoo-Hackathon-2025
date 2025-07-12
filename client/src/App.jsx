f984096e1d4b238496138dcc7bb6af4819440ac7
gvaishnavig <vaishnavi.gurram.19@gmail.com>
2025-07-12 12:09:17 +0530

    page structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import ViewProfile from "./pages/ViewProfile";
+import SwapRequestForm from "./pages/SwapRequestForm";
+import SwapRequests from "./pages/SwapRequests";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile/:userId" element={<Profile />} />
+        <Route path="/view/:userId" element={<ViewProfile />} />
+        <Route path="/request/:userId" element={<SwapRequestForm />} />
+        <Route path="/swap-requests" element={<SwapRequests />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
+
