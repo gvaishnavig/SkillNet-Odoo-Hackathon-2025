d69d3f1c2d93f3ad98673a529f087f9d7b5629cc
gvaishnavig <vaishnavi.gurram.19@gmail.com>
2025-07-12 10:40:59 +0530

    framework

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-pink-500 to-purple-600 text-white text-4xl font-bold">
+      🎉 Tailwind is working!
+    </div>
+  );
+}
+
+export default App;
